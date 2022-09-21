a60b57f76b1d9af5accf44c114cc736247e78c4f
Julian <julian.deronja@student.htldornbirn.at>
2022-09-21 11:53:26 +0200

    if-clauses

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Random random = new Random();
+
+        int randomNumber = random.nextInt(100);
+
+        if (randomNumber <= 20) {
+            System.out.println("Mini");
+        } else if (randomNumber > 20 && randomNumber < 50) {
+            System.out.println("Medi");
+        } else {
+            System.out.println("Max");
+        }
     }
 }
