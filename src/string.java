d389e4ba638c75634984d5c4d0b09983e8ab7ffa
Julian <julian.deronja@student.htldornbirn.at>
2022-10-13 09:35:04 +0200

    array and cesar watched videos and tried to solve and come along

---
+++ src/string.java
@@ -0,0 +1,23 @@
+public class string {
+
+    public static void main(String[] args) {
+
+        int[] throwsArray = new int[6];
+
+       throwsArray[5] = 8;
+
+        for (int i = 0; i < throwsArray.length; i++) {
+            System.out.print(throwsArray[i]);
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
