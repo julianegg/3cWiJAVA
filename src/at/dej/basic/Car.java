d997fcf4544e68820db00a0ea27a268933d02aeb
Julian <julian.deronja@student.htldornbirn.at>
2022-12-01 09:31:11 +0100

    03 worked

--- src/at/dej/basic/Car.java
+++ src/at/dej/basic/Car.java
@@ -2,18 +2,41 @@ package at.dej.basic;
 
 public class Car {
 
-   public String color;
-   public String serialNumber;
-   public int  fuelConsumption;
-   public int fuelAmount;
+    public String color;
+    public String serialNumber;
+    public double fuelConsumption;
+    public double fuelAmount;
+    public int speed;
 
-   public void break(){
-      System.out.println("ich bremse");
-   }
+    public int fuelVolume;
+
+    public int valueOfFuel;
+
+    public void doBreak() {
+        this.speed = 0;
+        System.out.println(speed + "km/h");
+        System.out.println("i am break ");
+
+    }
 
 
-   public void drive(){
-      this.fuelAmount = this.fuelAmount-this.fuelConsumption;
-      System.out.println("I am driving");
-   }
+    public void drive() {
+        this.speed = 100;
+        this.fuelAmount = this.fuelAmount - this.fuelConsumption;
+        System.out.println(speed + "km/h");
+        System.out.println("I am driving");
+
+    }
+
+    public void turboBoost() {
+        int fillStand = (int) (fuelAmount / fuelVolume * 100.0);
+
+        if (fillStand >
+                10) {
+            System.out.println("Superboost");
+
+        } else {
+            System.out.println("Not enough fuel to go to Superboost");
+        }
+    }
 }
--- src/at/dej/basic/Main.java
+++ src/at/dej/basic/Main.java
@@ -1,29 +1,28 @@
 package at.dej.basic;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         int a = 7;
 
-    Car c1 = new Car();
-    c1.color = "blue";
-    c1.fuelConsumption = 6;
-    c1.serialNumber= "B123";
-    c1.fuelAmount = 90;
+        Car c1 = new Car();
+        c1.color = "blue";
+        c1.fuelConsumption = 7;
+        c1.serialNumber = "B123";
+        c1.fuelAmount = 20;
+        c1.fuelVolume = 50;
+        c1.turboBoost();
 
 
-
-
-    Car c2 = new Car();
+        Car c2 = new Car();
         c2.color = "blue";
         c2.fuelConsumption = 9;
-        c2.serialNumber= "M123";
+        c2.serialNumber = "M123";
         c2.fuelAmount = 80;
 
 
 
-        System.out.println(c2.fuelAmount);
-        c2.drive();
-        System.out.println(c2.fuelAmount);
+
+
     }
 }
